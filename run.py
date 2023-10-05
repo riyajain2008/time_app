f519ae63742efcba0c1fcdefbec004023f659ec0
Riya Jain <72738274+riyajain2008@users.noreply.github.com>
2023-10-05 15:46:13 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask
-import datetime
+from datetime import datetime  
+import pytz  
+
 app = Flask(__name__)
 
 
@@ -7,11 +9,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
-
-@app.route('/time')
+@app.route('/time')  
 def current_time():
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return 'Current time: {}'.format(current_time)
+    new_york_tz = pytz.timezone('America/New_York')
+    now = datetime.now(new_york_tz)
+    current_time = now.strftime("%Y-%m-%d %H:%M:%S")  
+    return 'Current time in New York: {}'.format(current_time)
 
 
 app.run(host='0.0.0.0',
